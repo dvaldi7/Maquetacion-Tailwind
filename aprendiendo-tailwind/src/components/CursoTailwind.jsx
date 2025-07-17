f8ef49afdcfe1c1dc0161e46cd5536cd705c3e71
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-07-17 19:37:02 +0200

    created project to learn tailwind with vite+react

---
+++ aprendiendo-tailwind/src/components/CursoTailwind.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CursoTailwind = () => {
+  return (
+    <>
+        <h1>Prueba con Tailwind</h1>
+    </>
+  )
+}
---
+++ aprendiendo-tailwind/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { CursoTailwind } from './components/CursoTailwind.jsx'
+
+createRoot(document.getElementById('root')).render(
+
+  <CursoTailwind />
+)
