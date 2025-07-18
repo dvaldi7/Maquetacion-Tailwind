8c5fea397a8f4b5670e7b7f150629a99663494a9
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-07-18 21:14:32 +0200

    added pattern to background

--- aprendiendo-tailwind/src/components/CursoTailwind.jsx
+++ aprendiendo-tailwind/src/components/CursoTailwind.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export const CursoTailwind = () => {
   return (
-    <>
-        <h1>Prueba con Tailwind</h1>
-    </>
+    <div className='bg-body-pattern'>
+        <h1 className='text-4xl bg-black m-5 p-5 text-center text-yellow-500'>Prueba con Tailwind</h1>
+        <p>Hola 1</p>
+        <p>Hola 2</p>
+        <p>Hola 3</p>
+        <p>Hola 4</p>
+        <p>Hola 5</p>
+       
+    </div>
   )
 }
